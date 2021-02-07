bd2af166bb5eb886fe9676dcbd98950fc5d58f28
Camila Arancibia <camilachillan99@gmail.com>
2021-02-06 23:44:48 -0300

    HideShow () created, needs correction

--- myscript.js
+++ myscript.js
@@ -2,3 +2,12 @@ function myFunction() {
     var name = window.prompt("¿Cómo te llamas? ");
     alert("Gracias por ver mi portafolio, \n " + name);
 }
+function hideShow(){
+    if(document.querySelector('.menu').classList.contains('is-active')){
+        document.querySelector('.menu').classList.remove('is-active');
+    }
+    else{
+        document.querySelector('.menu').classList.add('is-active');
+    }
+    
+}
