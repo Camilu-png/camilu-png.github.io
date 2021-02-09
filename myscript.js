d3b4e15857a68fa5d5e1e24420e0b50e08f80127
Camila Arancibia <camilachillan99@gmail.com>
2021-02-08 22:00:51 -0300

    Menu bug fixed

--- myscript.js
+++ myscript.js
@@ -5,9 +5,13 @@ function myFunction() {
 function hideShow(){
     if(document.querySelector('.menu').classList.contains('is-active')){
         document.querySelector('.menu').classList.remove('is-active');
+        document.querySelector('.fa-folder').classList.add('is-active');
+        document.querySelector('.fa-folder-open').classList.remove('is-active');
     }
     else{
         document.querySelector('.menu').classList.add('is-active');
+        document.querySelector('.fa-folder').classList.remove('is-active');
+        document.querySelector('.fa-folder-open').classList.add('is-active');
     }
     
 }
